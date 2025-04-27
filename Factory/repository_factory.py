2a371dcf3bcae5907a539b59706ca0b868598b7a
Nkosikhona Mbatha <Nkosikhonankosi760@gmail.com>
2025-04-27 23:20:09 +0200

    Update repository_factory.py

--- Factory/repository_factory.py
+++ Factory/repository_factory.py
@@ -1,1 +1,13 @@
+from repositories.inmemory.in_memory_student_repository import InMemoryStudentRepository
+# Future import: from repositories.filesystem.file_system_student_repository import FileSystemStudentRepository
+
+class RepositoryFactory:
+    @staticmethod
+    def get_student_repository(storage_type: str):
+        if storage_type == "MEMORY":
+            return InMemoryStudentRepository()
+        # elif storage_type == "FILE":
+        #     return FileSystemStudentRepository("/path/to/storage.json")
+        else:
+            raise ValueError(f"Invalid storage type: {storage_type}")
 
